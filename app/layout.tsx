fba1d59a2c44240aeb03a9322fe678b8e9eed6ae
Farrukh Ahmed <92082052+Farrukh-Ahmed1@users.noreply.github.com>
2023-07-03 14:11:48 +0500

    Add grafbase config

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import 'app/globals.css';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+
+export const metadata = {
+  title: "Dribble Clone",
+  description: "Showcase & discover remakable developer projects.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
